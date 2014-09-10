c7302fb54695f19d11337b8f5c79dd2991cc1bf0
Leonel Garza <JuanGarza@Leonels-MacBook-Pro.local>
2014-09-10 16:12:05 -0500

    added a test file

---
+++ PGholo.h
@@ -0,0 +1,13 @@
+//
+//  PGholo.h
+//  prueba
+//
+//  Created by Leonel Garza on 9/10/14.
+//  Copyright (c) 2014 Leonel Garza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGholo : NSObject
+
+@end
---
+++ PGholo.m
@@ -0,0 +1,13 @@
+//
+//  PGholo.m
+//  prueba
+//
+//  Created by Leonel Garza on 9/10/14.
+//  Copyright (c) 2014 Leonel Garza. All rights reserved.
+//
+
+#import "PGholo.h"
+
+@implementation PGholo
+
+@end
